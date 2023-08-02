112c380cf678354daa3190768d0363838465dc22
Greg Davis <gregddavis@protonmail.com>
2023-08-02 13:44:35 +1000

    VS 2022 project files

--- src/os/windows/pws_autotype/pws_at_version.h
+++ src/os/windows/pws_autotype/pws_at_version.h
@@ -6,8 +6,8 @@
  * http://www.opensource.org/licenses/artistic-license-2.0.php
  */
 
-#ifndef _PWS_OSK_VERSION_H_
-#define _PWS_OSK_VERSION_H_
+#ifndef _PWS_AT_VERSION_H_
+#define _PWS_ATVERSION_H_
 
 // version.h is automatically generated from version.in by a custom
 // pre-build step that uses git's 'describe' function and
@@ -17,10 +17,10 @@
 //   Revision  = 0 for all Formally Released versions
 //   Revision != 0 for all Intermediate versions
 
-#define PRODUCTVER         @pwsafe_osk_PRODUCTVER@
-#define STROSKPRODUCTVER   "@pwsafe_osk_PRODUCTVER@, @pwsafe_osk_VERSTRING@\0"
+#define PRODUCTVER         1
+#define STRATPRODUCTVER    "1\0"
 
-#define FILEVER        @pwsafe_osk_PRODUCTVER@
-#define STROSKFILEVER  "@pwsafe_osk_PRODUCTVER@, @pwsafe_osk_VERSTRING@\0"
+#define FILEVER        1
+#define STRATFILEVER   "1\0"
 
-#endif // _PWS_OSK_VERSION_H_
+#endif // _PWS_AT_VERSION_H_
